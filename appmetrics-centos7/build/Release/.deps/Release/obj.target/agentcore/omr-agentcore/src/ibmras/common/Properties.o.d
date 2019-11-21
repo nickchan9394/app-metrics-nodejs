feb804afa7af195c96b95dfca4e47cb5a0e8b176
Nick Chen <Nick_Chen@epam.com>
2019-11-21 16:13:13 +0800

    add centos 7 version

---
+++ appmetrics-centos7/build/Release/.deps/Release/obj.target/agentcore/omr-agentcore/src/ibmras/common/Properties.o.d
@@ -0,0 +1,10 @@
+cmd_Release/obj.target/agentcore/omr-agentcore/src/ibmras/common/Properties.o := g++ '-DEXPORT' '-DIBMRAS_DEBUG_LOGGING' '-DOMR_NODEJS' '-DNODE_GYP_MODULE_NAME=agentcore' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-D_LINUX' '-DLINUX' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DOPENSSL_NO_PINSHARED' -I../omr-agentcore/src -I../omr-agentcore/org.eclipse.paho.mqtt.c/src -I/root/.cache/node-gyp/10.17.0/include/node -I/root/.cache/node-gyp/10.17.0/src -I/root/.cache/node-gyp/10.17.0/deps/openssl/config -I/root/.cache/node-gyp/10.17.0/deps/openssl/openssl/include -I/root/.cache/node-gyp/10.17.0/deps/uv/include -I/root/.cache/node-gyp/10.17.0/deps/zlib -I/root/.cache/node-gyp/10.17.0/deps/v8/include  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -O3 -fno-omit-frame-pointer -fno-rtti -std=gnu++1y -MMD -MF ./Release/.deps/Release/obj.target/agentcore/omr-agentcore/src/ibmras/common/Properties.o.d.raw   -c -o Release/obj.target/agentcore/omr-agentcore/src/ibmras/common/Properties.o ../omr-agentcore/src/ibmras/common/Properties.cpp
+Release/obj.target/agentcore/omr-agentcore/src/ibmras/common/Properties.o: \
+ ../omr-agentcore/src/ibmras/common/Properties.cpp \
+ ../omr-agentcore/src/ibmras/common/Properties.h \
+ ../omr-agentcore/src/ibmras/monitoring/AgentExtensions.h \
+ ../omr-agentcore/src/ibmras/common/util/strUtils.h
+../omr-agentcore/src/ibmras/common/Properties.cpp:
+../omr-agentcore/src/ibmras/common/Properties.h:
+../omr-agentcore/src/ibmras/monitoring/AgentExtensions.h:
+../omr-agentcore/src/ibmras/common/util/strUtils.h:
